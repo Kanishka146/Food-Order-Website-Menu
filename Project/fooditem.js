c93031fb0994704590d93f2fb938e1518d8deee8
Kanishka Patel <115797550+Kanishka146@users.noreply.github.com>
2023-04-04 22:36:05 +0530

    Add files via upload

---
+++ Project/fooditem.js
@@ -0,0 +1,486 @@
+const foodItem=[
+    {
+        id:1,
+        name:'Kadhai Paneer',
+        category:'main course',
+        rating:0,
+        price:150,
+        img:'images/kadahai_paneer.jpg',
+        quantity:1
+},
+{
+    id:2,
+    name:'Shahi Paneer',
+    category:'main course',
+    rating:0,
+    price:150,
+    img:'images/shahipanner.jpg',
+    quantity:1
+},
+ {
+    id:3,
+    name:'Paneer Butter',
+    category:'main course',
+    rating:0,
+    price:150,
+    img:'images/pannerButterMasala.jpg',
+    quantity:1
+},
+ {
+    id:4,
+    name:'Dal Makhani',
+    category:'main course',
+    rating:0,
+    price:130,
+    img:'images/dalMakhani.jpg',
+    quantity:1
+},
+ {
+    id:5,
+    name:'Matar Paneer',
+    category:'main course',
+    rating:0,
+    price:150,
+    img:'images/matarpaneer.jpg',
+    quantity:1
+}, 
+{
+    id:6,
+    name:'Paneer Tikka',
+    category:'main course',
+    rating:0,
+    price:150,
+    img:'images/paneertikka.jpg',
+    quantity:1
+},
+ {
+    id:7,
+    name:'Chaap Masala',
+    category:'main course',
+    rating:0,
+    price:130,
+    img:'images/chaapmasala.jpg',
+    quantity:1
+},
+ {
+    id:8,
+    name:'Paneer Layav Dar',
+    category:'main course',
+    rating:0,
+    price:150,
+    img:'images/paneerlayabdar.jpg',
+    quantity:1
+}, 
+{
+    id:9,
+    name:'Plain Roti',
+    category:'tandoori',
+    rating:0,
+    price:10,
+    img:'images/plainroti.jpg',
+    quantity:1
+},
+ {
+    id:10,
+    name:'Butter Roti',
+    category:'tandoori',
+    rating:0,
+    price:15,
+    img:'images/butterroti.jpg',
+    quantity:1
+},
+{
+    id:11,
+    name:'Plain Nan',
+    category:'tandoori',
+    rating:0,
+    price:20,
+    img:'images/plainnan.jpg',
+    quantity:1
+},
+{
+    id:12,
+    name:'Butter Nan',
+    category:'tandoori',
+    rating:0,
+    price:30,
+    img:'images/butternan.jpg',
+    quantity:1
+},
+{
+    id:13,
+    name:'Aloo Paratha',
+    category:'tandoori',
+    rating:0,
+    price:50,
+    img:'images/alooparatha.jpg',
+    quantity:1
+},
+{
+    id:14,
+    name:'Paneer Paratha',
+    category:'tandoori',
+    rating:0,
+    price:60,
+    img:'images/paneerparatha.jpg',
+    quantity:1
+},
+{
+    id:15,
+    name:'Aloo Kulcha',
+    category:'tandoori',
+    rating:0,
+    price:50,
+    img:'images/alookulcha.jpg',
+    quantity:1
+},
+{
+    id:16,
+    name:'Paneer Tikka',
+    category:'tandoori',
+    rating:0,
+    price:150,
+    img:'images/pt2.jpg',
+    quantity:1
+},
+{
+    id:17,
+    name:'Tandoori Chaap',
+    category:'tandoori',
+    rating:0,
+    price:100,
+    img:'images/tandoorisoyachaap.jpg',
+    quantity:1
+},
+{
+    id:18,
+    name:'Malai Chaap',
+    category:'tandoori',
+    rating:0,
+    price:120,
+    img:'images/malaichaap.jpg',
+    quantity:1
+},
+{
+    id:19,
+    name:'French Fries',
+    category:'continental',
+    rating:0,
+    price:55,
+    img:'images/frenchfries.jpg',
+    quantity:1
+},
+{
+    id:20,
+    name:'Pasta',
+    category:'continental',
+    rating:0,
+    price:85,
+    img:'images/pasta.jpg',
+    quantity:1
+},
+{
+    id:21,
+    name:'Veg Burger',
+    category:'continental',
+    rating:0,
+    price:35,
+    img:'images/vegburger.jpg',
+    quantity:1
+},
+{
+    id:22,
+    name:'cheese Burger',
+    category:'continental',
+    rating:0,
+    price:45,
+    img:'images/cburger.jpg',
+    quantity:1
+},
+{
+    id:23,
+    name:'Hot Dog',
+    category:'continental',
+    rating:0,
+    price:35,
+    img:'images/hotdog.jpg',
+    quantity:1
+},
+{
+    id:24,
+    name:'Sandwich',
+    category:'continental',
+    rating:0,
+    price:35,
+    img:'images/sandwich.jpg',
+    quantity:1
+},
+{
+    id:25,
+    name:'Cream Roll',
+    category:'continental',
+    rating:0,
+    price:35,
+    img:'images/creamroll.jpg',
+    quantity:1
+},
+{
+    id:26,
+    name:'Aloo Patty',
+    category:'continental',
+    rating:0,
+    price:25,
+    img:'images/patty.jpg',
+    quantity:1
+},
+{
+    id:27,
+    name:'Eggless Cakes',
+    category:'continental',
+    rating:0,
+    price:305,
+    img:'images/cake.jpg',
+    quantity:1
+},
+{
+    id:28,
+    name:'Eggless Pastries',
+    category:'continental',
+    rating:0,
+    price:35,
+    img:'images/pastries.jpg',
+    quantity:1
+},
+{
+    id:29,
+    name:'Cold Drink',
+    category:'beverages',
+    rating:0,
+    price:35,
+    img:'images/colddrink.jpg',
+    quantity:1
+},
+{
+    id:30,
+    name:'Cold Drink can',
+    category:'beverages',
+    rating:0,
+    price:40,
+    img:'images/can.jpg',
+    quantity:1
+},
+{
+    id:31,
+    name:'Frooti/Appy/String',
+    category:'beverages',
+    rating:0,
+    price:30,
+    img:'images/sting.jpg',
+    quantity:1
+},
+{
+    id:32,
+    name:'Lassi/Chach',
+    category:'beverages',
+    rating:0,
+    price:30,
+    img:'images/lassi.jpg',
+    quantity:1
+},
+{
+    id:33,
+    name:'Cold Coffee',
+    category:'beverages',
+    rating:0,
+    price:40,
+    img:'images/coldcoffee.jpg',
+    quantity:1
+},
+{
+    id:34,
+    name:'Juice',
+    category:'beverages',
+    rating:0,
+    price:30,
+    img:'images/juice.jpg',
+    quantity:1
+},
+{
+    id:35,
+    name:'Chocolate',
+    category:'beverages',
+    rating:0,
+    price:50,
+    img:'images/chocolate.jpg',
+    quantity:1
+},
+{
+    id:36,
+    name:'Ice cream',
+    category:'beverages',
+    rating:0,
+    price:40,
+    img:'images/icecream.jpg',
+    quantity:1
+},
+{
+    id:37,
+    name:'Lays/Kurkure',
+    category:'beverages',
+    rating:0,
+    price:20,
+    img:'images/kurkure.jpg',
+    quantity:1
+},
+{
+    id:38,
+    name:'Tea',
+    category:'beverages',
+    rating:0,
+    price:15,
+    img:'images/tea.jpg',
+    quantity:1
+},
+{
+    id:39,
+    name:'Chole Bhature',
+    category:'indian snacks',
+    rating:0,
+    price:85,
+    img:'images/cb.jpg',
+    quantity:1
+},
+{
+    id:40,
+    name:'Chole Chawal',
+    category:'indian snacks',
+    rating:0,
+    price:85,
+    img:'images/cc.jpg',
+    quantity:1
+},
+{
+    id:41,
+    name:'Pav Bhaji',
+    category:'indian snacks',
+    rating:0,
+    price:75,
+    img:'images/pb.jpg',
+    quantity:1
+},
+{
+    id:42,
+    name:'Patty',
+    category:'indian snacks',
+    rating:0,
+    price:25,
+    img:'images/p.jpg',
+    quantity:1
+},
+{
+    id:43,
+    name:'Hakka Noodles',
+    category:'c',
+    rating:0,
+    price:65,
+    img:'images/hn.jpg',
+    quantity:1
+},
+{
+    id:44,
+    name:'Fried Rice',
+    category:'c',
+    rating:0,
+    price:85,
+    img:'images/fr.jpg',
+    quantity:1
+},{
+    id:45,
+    name:'Manchurian',
+    category:'c',
+    rating:0,
+    price:75,
+    img:'images/m.jpg',
+    quantity:1
+},{
+    id:46,
+    name:'Chilli Panner',
+    category:'c',
+    rating:0,
+    price:120,
+    img:'images/cp.jpg',
+    quantity:1
+},{
+    id:47,
+    name:'Chilli Potato',
+    category:'c',
+    rating:0,
+    price:75,
+    img:'images/cp2.jpg',
+    quantity:1
+},
+{
+    id:48,
+    name:'Honey Chilli Potato',
+    category:'c',
+    rating:0,
+    price:75,
+    img:'images/hcp.jpg',
+    quantity:1
+},
+{
+    id:49,
+    name:'Spring Roll',
+    category:'c',
+    rating:0,
+    price:75,
+    img:'images/sr.jpg',
+    quantity:1
+},
+{
+    id:50,
+    name:'Momos Steam/Fried',
+    category:'c',
+    rating:0,
+    price:55,
+    img:'images/mommo.jpg',
+    quantity:1
+},
+{
+    id:51,
+    name:'Macaroni',
+    category:'c',
+    rating:0,
+    price:55,
+    img:'images/mc.jpg',
+    quantity:1
+},
+{
+    id:52,
+    name:'Maggi',
+    category:'c',
+    rating:0,
+    price:60,
+    img:'images/maggi.jpg',
+    quantity:1
+},
+{
+    id:53,
+    name:'Masala Dosa',
+    category:'south-indian',
+    rating:0,
+    price:75,
+    img:'images/md.jpg',
+    quantity:1
+},
+{
+    id:54,
+    name:'Paneer Dosa',
+    category:'south-indian',
+    rating:0,
+    price:85,
+    img:'images/pd.jpg',
+    quantity:1
+},
+]
+export {foodItem};
---
+++ Project/index.js
@@ -0,0 +1,304 @@
+import {foodItem} from './fooditem.js'
+
+function displayItems(){
+    var maincourse= document.getElementById('main-course');
+    var tandoori=  document.getElementById('tandoori');;
+    var continental=  document.getElementById('continental');
+    var beverages=  document.getElementById('beverages');
+    var indiansnacks=  document.getElementById('indian-snacks');
+    var chinese=  document.getElementById('chinese');
+    var southindian=  document.getElementById('south-indian');
+
+    const maincourseData= foodItem.filter((item)=>item.category=='main course');
+    const  tandooriData= foodItem.filter((item)=>item.category=='tandoori');
+    const continentalData= foodItem.filter((item)=>item.category=='continental');
+    const beveragesData= foodItem.filter((item)=>item.category=='beverages');
+    const indiansnacksData= foodItem.filter((item)=>item.category=='indian snacks');
+    const chineseData= foodItem.filter((item)=>item.category=='c');
+    const southindianData= foodItem.filter((item)=>item.category=='south-indian');
+
+    maincourseData.map(item=>{
+        
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        maincourse.appendChild(itemCard);
+        
+    })
+
+
+    tandooriData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+        
+        tandoori.appendChild(itemCard)
+
+    })
+
+    continentalData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+        
+        continental.appendChild(itemCard)
+
+    })
+
+    beveragesData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        
+        beverages.appendChild(itemCard)
+    
+    })
+
+    indiansnacksData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        
+        indiansnacks.appendChild(itemCard)
+        
+    })
+
+     chineseData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        chinese.appendChild(itemCard)
+
+    })
+
+    southindianData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : Rs. ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        southindian.appendChild(itemCard)
+
+    })
+}
+displayItems();
